56392cdf93d3ae844352102fcb5fcf25bfbc30c3
Ariel <salinas.ariel02@gmail.com>
2023-05-08 15:28:59 -0300

    class 5/8
    first step into react frontend

---
+++ proyect-g13/src/components/Login/Login.js
@@ -0,0 +1,8 @@
+function Login(){
+    return(
+        <div className="backplate">
+            <h3>Login</h3>
+        </div>
+    )
+}
+export default Login;
---
+++ proyect-g13/src/components/MenuBar/MenuBar.js
@@ -0,0 +1,27 @@
+import "./MenuBar.css"
+import Logo from "../../../src/Img/logoFootbal.png"
+
+function MenuBar()  {
+  return (
+    <nav  class=" navbar navbar-expand-lg navbar-light backcolor">
+      <div>
+        <img class="high" src={Logo} />
+        </div>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+     <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class=" collapse navbar-collapse pad" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="#"><b>Home</b> <span class="sr-only"></span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><b>Reservar</b></a>
+        </li>
+        
+      </ul>
+    </div>
+  </nav>
+  );
+}
+export default MenuBar;
