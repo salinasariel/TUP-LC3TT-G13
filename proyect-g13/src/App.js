1159bb77878849e2f75f57faabf7648022aaaea4
Antonio Torralba <antoniotorralba0403@gmail.com>
2023-05-08 20:51:24 -0300

    Login
    First step in login

--- proyect-g13/src/App.js
+++ proyect-g13/src/App.js
@@ -1,14 +1,20 @@
-
 import './App.css';
-
 import MenuBar from './components/MenuBar/MenuBar';
-import LogIn from './components/Login/Login';
+import Login from './components/Login/Login';
+
+import { useState } from 'react';
 
 function App() {
+const [user, setUser] = useState([])
+
   return (
-    
-    
-    <MenuBar/>
+    <div className='App'>
+      {
+        !user.length > 0
+          ? <Login setUser={setUser} />
+          : <MenuBar />
+      }
+    </div>
   );
 }
 
--- proyect-g13/src/components/Login/Login.js
+++ proyect-g13/src/components/Login/Login.js
@@ -1,8 +1,43 @@
-function Login(){
+import "./Login.css"
+
+import { useState } from "react"
+
+
+export function Login({setUser}){
+const [name, setName] = useState("")
+const [password, setPassword] = useState("")
+const[error, setError] = useState(false)
+
+
+const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if(name === "" || password === ""){
+        setError(true)
+        return
+    }
+
+    setError(false)
+
+    setUser([name])
+}
+
     return(
-        <div className="backplate">
-            <h3>Login</h3>
-        </div>
+        <section>
+
+            <form className="form"
+            onSubmit={handleSubmit}>
+                <h1>login</h1>
+                <input type="text"
+                value={name}
+                onChange={e => setName(e.target.value)}></input>
+                <input type="password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}></input>
+                <button>Login</button>
+            </form>
+            {error && <p>todos los campos son obligatorios</p>}
+        </section>
     )
 }
 export default Login;
