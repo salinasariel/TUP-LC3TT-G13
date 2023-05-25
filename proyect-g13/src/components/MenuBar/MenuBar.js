5ae8a68494bad50482821365f75e42adaad6bc9a
Ariel <salinas.ariel02@gmail.com>
2023-05-25 14:29:09 -0300

    correction responsive bug

--- proyect-g13/src/components/MenuBar/MenuBar.js
+++ proyect-g13/src/components/MenuBar/MenuBar.js
@@ -9,7 +9,9 @@ function MenuBar()  {
         <img class="high" src={Logo} />
         </div>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      
      <span class="navbar-toggler-icon"></span>
+     
     </button>
     <div class=" collapse navbar-collapse pad" id="navbarNav">
       <ul class="navbar-nav">
