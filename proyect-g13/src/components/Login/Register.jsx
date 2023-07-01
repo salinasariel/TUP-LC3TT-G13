301a6da0d816c59d20858446b554edf38da7c375
Antonio Torralba <antoniotorralba0403@gmail.com>
2023-07-01 18:16:45 -0300

    finish login and register

--- proyect-g13/src/components/Login/Register.jsx
+++ proyect-g13/src/components/Login/Register.jsx
@@ -1,72 +1,15 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import MenuBarLogin from '../MenuBar/MenuBarLogin'
 import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 
-//function Register() {
-   // const [email, setEmail] = useState("")
-   // const [name, setName] = useState("")
-   // const [password, setPassword] = useState("")
-   // const navigate = useNavigate()
-    
-    //const handleOnSubmit = (e) =>{
-       // e.preventDefault()
-        //debugger;
-       // let registerNeeded = {email, password}
-        
-       // fetch("https://644bfc2317e2663b9dfd613c.mockapi.io/api/v1/users", {
-           // method:"POST",
-           // headers:{'content-type':'application/json'},
-           // body:JSON.stringify(registerNeeded)
-      //  }).then((response)=>{
-          //  toast.success('Registrado satisfactoriamente')
-           // navigate('/')
-       // }).catch(err => {
-          //  toast.error("Hubo un error :"+err.message)
-       // })
-
-
-   // }
-
-  //return (
-   // <>
-
-   //         <MenuBarLogin />
-    //        <section >
-
-   //             <form className="form scale-up-center" onSubmit={handleOnSubmit}>
-   //                 <h1 class="titelLog">Registrarme</h1>
-   //                 <input
-   //                  required
-   //                   value={email}
-   //                    onChange={e=> setEmail(e.target.value)}
-    //                    type="email" placeholder='Email' class="email form-control" />
-                    
-      //              <input
-      //               required
-      //                value={password}
-      //                 onChange={e=> setPassword(e.target.value)}
-       //                 placeholder="Contraseña" class="contrasenia form-control" aria-label="Default"                 aria-describedby="inputGroup-sizing-default" type="password"></input>
-       //             <button class="btn btn-light justify-content-center mt-2 ">Registrarse</button> <br></br>
-         //           <a  class="register ml-2" href="/">Ya tengo una cuenta</a>
-       //         </form>
-                
-       //     </section>
-      //  </>
- // )
-//}
-
-//import React, { useState } from 'react';
-
 const Register = () => {
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
-  const [passwordError, setPasswordError] = useState('');
-  const [passwordError2, setPasswordError2] = useState('');
-  const [passwordExito, setPasswordExito] = useState('')
   const [email, setEmail] = useState("")
   const navigate = useNavigate()
   const [role, setRole] = useState ('')
+  const roleRef = useRef(null)
 
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
@@ -84,55 +27,48 @@ const Register = () => {
 
   const handleOnSubmit = (e) => {
     e.preventDefault();
-    debugger;
-     let registerNeeded = {email, password,role}
+    if (handleValidation()) {
+      let registerNeeded = {email, password,role}
         
        fetch("https://644bfc2317e2663b9dfd613c.mockapi.io/api/v1/users", {
             method:"POST",
             headers:{'content-type':'application/json'},
             body:JSON.stringify(registerNeeded)
         }).then((response)=>{
-            toast.success('Registrado satisfactoriamente')
+          alert('Registrado satisfactoriamente')
             navigate('/')
         }).catch(err => {
-          //  toast.error("Hubo un error :"+err.message)
+          alert("Hubo un error :"+err.message)
         })
 
-
-    // Validar la contraseña
-    if (password.length < 6 ) {
-      setPasswordError('La contraseña no puede tener menos de 6 caracteres');
-      return;
-    }
-    if (password.length > 20 ) {
-        setPasswordError2('La contraseña no puede tener mas de 20 caracteres');
-        return;
-      }
-      
-    if (password !== confirmPassword) {
-      setPasswordError('Las contraseñas no coinciden');
-      return;
     }
 
-    if (!role && role ==='') {
-        setPasswordError('Por favor, selecciona tu rol');
-        return;
-      }
-
-      else if (password.length >=6 && password.length <= 20 ) {
-        setPasswordExito('Registrado Correctamente!');
-        return;
-
-      }
-
-
-    // Continuar con la lógica de registro si las validaciones son exitosas
-    // ...
+    
+    
   };
+
+  const handleValidation = () =>{
+    let result = true
+    if(password.length < 6 || password.length > 20  ) {
+      result = false
+      alert('La contraseña no puede tener menos de 6 caracteres y mas de 20');
+    }      
+    if (password !== confirmPassword) {
+      result = false
+      alert('Las contraseñas no coinciden');
+    }
+    if (role !== "2" && role !== "3") {
+      result = false
+      roleRef.current.focus()
+      alert("Seleccione un rol")
+    }
+    return result
+  }
+
 
   return (
 
-    <>
+      <>
 
             <MenuBarLogin />
             <section >
@@ -160,11 +96,6 @@ const Register = () => {
             />
         </div>
          
-          
-           
-          
-          
-        
         <div>
         <label htmlFor="confirmPassword"></label>
         <input
@@ -177,23 +108,19 @@ const Register = () => {
 
         <div>
         <label htmlFor="role" className='justify-content-end'></label>
-        <select id="role" className='btn btn-light justify-content-center mt-3' value={role} onChange={handleRoleChange}>
-          <option value="">Seleccionar rol</option>
-          <option value='2'>Dueño</option>
-          <option value='3'>Usuario</option>
+        <select ref={roleRef} id="role" className='btn btn-light justify-content-center mt-3' value={role} onChange={handleRoleChange}>
+          <option>Seleccione Rol</option>
+          <option value='2'>Usuario</option>
+          <option value='3'>Dueño</option>
         </select>
       </div>
 
       </div>
-      {passwordError && <div>{passwordError}</div>}
-      {passwordError2 && <div>{passwordError2}</div>}
-      {passwordExito && <div>{passwordExito}</div>}
       <button class="btn btn-light justify-content-center mt-3 " type="submit">Registrar</button>
     </form>
     </section>
 
     </>
-    
     
   );
 };
