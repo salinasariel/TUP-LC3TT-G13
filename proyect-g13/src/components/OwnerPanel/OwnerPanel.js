306ec77a61a09cb9403ab0cdf6a377b6003c2b92
Ariel <salinas.ariel02@gmail.com>
2023-07-01 21:05:38 -0300

    OP style but dont work

--- proyect-g13/src/components/OwnerPanel/OwnerPanel.js
+++ proyect-g13/src/components/OwnerPanel/OwnerPanel.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import MenuBar from '../MenuBar/MenuBar'
 
 function OwnerPanel() {
   return (
-    <div>OwnerPanel</div>
+    <div>
+      <MenuBar />
+      <div className="backplate scale-up-center">
+           
+            <h3>Panel de dueños.</h3>
+            <p>Aqui podra administar los turnos otorgados en su establecimiento.</p>
+            <button  className='btn btn-warning'>Administrar mis canchas</button><br></br>
+
+            <p>ADVERTENCIA: La suguiente funcion habilitara o desabilitara todos los turnos en todas las canchas de su establecimiento. Procure tener cuidado ya que puede cancelar turnos ya otorgados.</p>
+            <button  className='btn btn-success'>Habilitar dias</button>
+            <button  className='btn btn-danger'>Bloquear dias</button>
+            
+        </div>
+    </div>
   )
 }
 
